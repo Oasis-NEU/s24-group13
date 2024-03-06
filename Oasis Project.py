16fa4678285ba99ca9b23436811011b9e8bba5c9
rishikamtam12 <157946455+rishikamtam12@users.noreply.github.com>
2024-03-05 19:03:31 -0500

    Update Oasis Project.py

--- Oasis Project.py
+++ Oasis Project.py
@@ -9,6 +9,9 @@ from nba_api.stats.endpoints import playercareerstats
 from nba_api.stats.static import players
 
 
+
+
+
 def get_id():
     """
     Gets the player id based on the inputted player name, returns a list of 
@@ -16,6 +19,7 @@ def get_id():
     """
     
     team_ids = []
+    player_names = []
     
     total_players = 0
     
@@ -23,10 +27,10 @@ def get_id():
     while total_players <= 1:
           player_dictionary = players.find_players_by_full_name(input("Whos is a player on your team?\n"))
           team_ids.append(player_dictionary[0]["id"])
+          player_names.append(player_dictionary[0]["full_name"])
           total_players += 1
-         
-    return team_ids
-
+ 
+    return team_ids, player_names
     
 
 
@@ -48,33 +52,70 @@ def find_player(team_ids):
 
 
 
-# def get_stats(player_id):
-#     """
+def get_stats(dataframes):
+    """
+    Given a list of dataframes returns a dataframe of the averages of each
+    statistic that affects players fantasy points for all players
+    """
+
+  
+    df_current_season = []
     
-#     """
     
-#     row_index = 8
-#     column_name = 'GP'
-#     points = "PTS"
-#     assists = "AST"
-#     rebounds = "REB"
-#     blocks = "BLK"
-#     steals = "STL"
-#     turnover = "TOR"
-#     fg_made = "FGM"
-#     fg_attempted = "FGA"
-#     fg_missed = fg_attempted = fg_made
+    for df in dataframes:
+        current_season_data = df.iloc[-1]
+        games_played = current_season_data['GP']
+        points = current_season_data['PTS']
+        rebounds = current_season_data['REB']
+        assists = current_season_data['AST']
+        blocks = current_season_data['BLK']
+        steals = current_season_data['STL']
+        turnovers = current_season_data['TOV']
+        fg_made = current_season_data['FGM']
+        fg_attempted = current_season_data['FGA']
+        fg_missed = fg_attempted - fg_made
+        
+        
+        data_frame = {
+"Points" : [round(points/games_played, 1)], "Rebounds" : [round(rebounds/games_played, 1)],
+"Assists" : [round(assists/games_played, 1)], "Blocks" : [round(blocks/games_played, 1)], 
+"Steals" : [round(steals/games_played, 1)], "Turnovers" : [round(turnovers/games_played, 1)],
+"FG Missed" : [round(fg_missed/games_played, 1)]
+
+}
+    
+        df = pd.DataFrame(data_frame)
+        
+        
+        df_current_season.append(df)
+        
+        {"Jayson Tatum": df, "Jaylen Brown": df}
+        
+
+    return df_current_season
+    
+    
+    
+    
+    
+    
 
         
         
         
 def main():
     
-    team_ids = get_id()
+    # list of team_ids and player names
+    team_ids, player_names = get_id()
+    print(player_names)
     
-
+    # Career statistics dataframe
     player_dataframes = find_player(team_ids)
-    print(player_dataframes)
+   # print(player_dataframes)
+    
+    # Current season statistic dataframe
+    season_data = get_stats(player_dataframes)
+    print(season_data)
 
 
 
